66d30403ec65c3f783005e72a225a6e04a86b864
unknown <see199@gmail.com>
2022-05-10 19:33:42 +0800

    1. Add New Google Form function, to load user details into Google Form 2. Add Get Contact by Email Modal in Contact Modal 3. Dizang Plate Image Update

---
+++ application/controllers/Gforms.php
@@ -0,0 +1,109 @@
+<?php
+class Gforms extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('contact_model');
+		$this->load->library('session');
+		$this->load->helper(array('form', 'common_helper', 'url','email','language'));
+	}
+
+	public function index($form_id="") {
+
+        require_once APPPATH . "libraries/GVendor/autoload.php";
+
+        if($form_id != ""){
+        	$this->session->set_userdata('form_id', $form_id);
+        }
+
+        $client = new Google_Client();
+        $client->setAuthConfig(APPPATH . '/config/google-webservice-credential.json');
+        $client->setRedirectUri('http://localhost/admin/gforms');
+        $client->addScope("https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile");
+
+
+        // Send Client Request
+        $objOAuthService = new Google_Service_Oauth2($client);
+
+        
+        // Add Access Token to Session
+        if ($this->input->get('code')) {
+            $client->authenticate($this->input->get('code'));
+            $this->session->set_userdata('access_token', $client->getAccessToken());
+            header('Location: ' . filter_var('http://localhost/admin/gforms', FILTER_SANITIZE_URL));
+        }
+
+        // Set Access Token to make Request
+        if ($this->session->userdata('access_token')) {
+            $client->setAccessToken($this->session->userdata('access_token'));
+        }else {
+            $authUrl = $client->createAuthUrl();
+        }
+        
+        
+
+        // Get User Data from Google and store them in $data
+        if ($client->getAccessToken()) {
+
+            if ($client->isAccessTokenExpired()) {
+                $this->logout();
+            }
+
+            $userData = $objOAuthService->userinfo->get();
+            $data['userData'] = $userData;
+            $this->session->set_userdata('access_token', $client->getAccessToken());
+
+            // Query Database for existing users
+            $res = $this->contact_model->get_contact_by_email($userData->email);
+            if(sizeof($res) == 0){
+                //Redirect to un-prefilled GForm
+                $this->forms($this->session->userdata('form_id'),false,array());
+            }
+            else{
+            	//If found, prefilled GForm
+            	$this->forms($this->session->userdata('form_id'),true,$res);
+                
+            }
+        } else {
+
+        	// Auto Login
+            //$authUrl = $client->createAuthUrl();
+            //$data['authUrl'] = $authUrl;
+            //$this->load->view('gforms', $data);
+            redirect($authUrl,'refresh');
+        }
+    }
+
+    private function forms($form_id,$prefilled=false,$user_data=array()){
+
+    	switch ($form_id) {
+    		//「真佛道場卓越計畫」說明會
+    		case '1FAIpQLScnF8LMmuvBkMwhtVf028by2FolOPcCJ23PYsRlQdL52CSioQ':
+    			$url = "https://docs.google.com/forms/d/e/$form_id/viewform";
+    			if($prefilled)
+    				$url .= "?usp=pp_url&entry.317713988=".urlencode($user_data['chapter_name'])."&entry.1685206990=".urlencode($user_data['name_chinese'])."&entry.759580696=".urlencode($user_data['position'])."&entry.809719715=".urlencode($user_data['phone_mobile'])."";
+
+    			//echo $url;
+    			redirect($url);
+    			break;
+    		
+    		default:
+    			echo "Google Form Not Found!";
+    			break;
+    	}
+    }
+
+
+
+    // Unset session and logout
+    public function logout() {
+        $this->session->unset_userdata('access_token');
+        $this->session->sess_destroy();
+        redirect(base_url().'gforms');
+    }
+
+	
+}
+
+?>
--- application/models/Contact_model.php
+++ application/models/Contact_model.php
@@ -75,6 +75,22 @@ class Contact_model extends CI_Model {
 		return $result;
 	}
 
+	public function get_contact_by_email($email = ''){
+		$this->db = $this->load->database('local', TRUE);
+
+		$this->db->select('c.name_chinese, c.name_malay, c.nric, c.email, c.contact_id, m.position, c.name_dharma, c.phone_mobile, cp.name_chinese as chapter_name')
+				->where('c.email',$email)
+				->where('m.chapter_id <>','1') // 不顯示馬密總
+				->where('m.position <> ','會員') // 不顯示會員
+				->from('tbs_contact c')
+				->join('tbs_chapter_member m','c.contact_id = m.contact_id', 'left')
+				->join('tbs_chapter cp','cp.chapter_id = m.chapter_id', 'left');
+		$res = $this->db->get();
+
+		if($res->result_array()) return $res->result_array()[0];
+		else return array();
+	}
+
 	public function get_contact_by_nric($nric = ''){
 		$this->db = $this->load->database('local', TRUE);
 

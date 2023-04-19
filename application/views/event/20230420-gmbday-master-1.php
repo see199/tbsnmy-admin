a5f6a54c9b64b0f3fe46172ad8fd0c7fbd86a6f0
Eyrique Wong <see199@gmail.com>
2023-04-19 20:14:42 +0800

    fix size

--- application/views/event/20230420-gmbday-master-1.php
+++ application/views/event/20230420-gmbday-master-1.php
@@ -227,7 +227,7 @@
 
 <body>
 
-    <div class="col-xs-12 col-sm-8 col-md-6 col-lg-6 col-sm-offset-2 col-md-offset-3 col-lg-offset-3">
+    <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
         <br />
                 <div class='row text-center'>
                     <div class='col-lg-10 col-lg-offset-1 col-xs-10 col-xs-offset-1'>

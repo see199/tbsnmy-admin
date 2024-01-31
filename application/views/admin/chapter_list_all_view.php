51b9a847597a9207be86e0448b8913de978ebe3a
Eyrique Wong <see199@gmail.com>
2024-01-31 16:12:25 +0800

    Chapter List All - add new column in DB 真佛編號 & 分堂代碼, and display in list

--- application/views/admin/chapter_list_all_view.php
+++ application/views/admin/chapter_list_all_view.php
@@ -15,19 +15,23 @@
                         <tr class="info">
                             <td><b>道場</b></td>
                             <td><b>馬密總<br />會員編號</b></td>
+                            <td><b>真佛<br />編號</b></td>
+                            <td><b>分堂<br />代碼</b></td>
                             <td><b>狀況</b></td>
                             <td><b>備註</b></td>
                         </tr>
                     </thead>
                     <tbody>
                         <?php foreach($contact as $state => $clist): ?>
-                        <tr class="success"><td colspan="4"><strong><?=$state . ' (' . count($clist) . ')';?></strong></td></tr>
+                        <tr class="success"><td colspan="6"><strong><?=$state . ' (' . count($clist) . ')';?></strong></td></tr>
                             <?php foreach($clist as $chapter_id => $c): ?>
                             <tr>
                                 <td nowrap>
                                     <a href="<?= base_url("admin/index/update_default_chapter/".$c['url_name']."/chapter_page"); ?>"><?= $c['name_chinese'];?></a>
                                 </td>
                                 <td nowrap><?= ($c['membership_id']) ? $c['membership_id'] : '非會員'; ?></td>
+                                <td nowrap><?= $c['tb_id']; ?></td>
+                                <td nowrap><?= $c['tb_chapter_id']; ?></td>
                                 <td nowrap><?= $this->config->item('tbs')['chapter_status'][$c['status']]; ?></td>
                                 <td width=70%><?php if($c['remarks']): ?><textarea class='form-control' rows=5 readonly><?= $c['remarks'];?></textarea><?php endif;?></td>
                             </tr>

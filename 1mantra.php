86172c5cc2e4637dea87536de0a385d0c62df34e
see199 <see199@gmail.com>
2025-10-04 17:22:50 +0800

    一生一咒 update

--- 1mantra.php
+++ 1mantra.php
@@ -1,20 +1,12 @@
 <?php
 
-function get_first_day($day_number=1, $month=false, $year=false){
-	$month  = ($month === false) ? strftime("%m"): $month;
-    $year   = ($year === false) ? strftime("%Y"): $year;
-
-    $first_day = 1 + ((7+$day_number - strftime("%w", @mktime(0,0,0,$month, 1, $year)))%7);
-
-    return @mktime(0,0,0,$month, $first_day, $year);
-}
-
-  // this will output the first saturday of january 2007 (Sat 06-01-2007)
-  $start_date = strftime("%Y-%m-%d", get_first_day(7, date('m')-1, date('Y')));
-  $end_date   = strftime("%Y-%m-%d", get_first_day(6, date('m'), date('Y')));
-
-  $start_date_name = strftime("%m%d", get_first_day(7, date('m')-1, date('Y')));
-  $end_date_name   = strftime("%m%d", get_first_day(6, date('m'), date('Y')));
+  // Days calculator
+  $day01_timestamp = strtotime('first day of last month');
+  $day31_timestamp = strtotime('last day of last month');
+  $start_date      = date('Y-m-d', $day01_timestamp);
+  $end_date        = date('Y-m-d', $day31_timestamp);
+  $start_date_name = date('md', $day01_timestamp);
+  $end_date_name   = date('md', $day31_timestamp);
 
 
   $url = "https://ch.tbsn.org/control/prayclass/exceloutput.html?pid=3&start_day=".$start_date."&end_day=".$end_date."&filetype=xls&datatype=";
@@ -23,13 +15,15 @@ function get_first_day($day_number=1, $month=false, $year=false){
   $url_1mil = $url."100";
   $m = date('m')-1;
 
-$mail_subject = "「" . $m . "月一生一咒」名單";
+$mail_subject = "「{$m}月一生一咒」名單";
 $mail_body = "尊敬的上師/法師您好，
 
-附上".$m."月的「一生一咒」兩份名單，麻煩幫忙列印呈給師尊加持。
+您好！文宣處附上{$m}月的「一生一咒」持誦名單，敬請打印後請師尊在護摩法會上加持。
 
-感恩！
-文宣處合十";
+謝謝！
+
+敬祝法務順利
+宗委會文宣處合十";
 
 ?>
 <h3><u>DATE: [<?=$start_date;?>] - [<?=$end_date;?>]</u></h3>
@@ -38,7 +32,7 @@ $mail_body = "尊敬的上師/法師您好，
  | <a href="<?=$url_1mil;?>">1MIL</a></p>
  <p><input value="一生一咒-<?=$start_date_name;?>-<?=$end_date_name;?>-100k" onfocus="this.select();"></p>
  <p><input value="一生一咒-<?=$start_date_name;?>-<?=$end_date_name;?>-1mil" onfocus="this.select();"></p>
- <p><input type="text" value="「<?=$mail_subject;?>月一生一咒」名單" onfocus="this.select();"></p>
- <p><textarea cols="100" rows="7" onfocus="this.select();"><?=$mail_body;?></textarea></p>
+ <p><input type="text" value="<?=$mail_subject;?>" onfocus="this.select();"></p>
+ <p><textarea cols="100" rows="10" onfocus="this.select();"><?=$mail_body;?></textarea></p>
 <a target="email" href="mailto:info@tbs-rainbow.org?subject=<?= $mail_subject;?>&body=<?= urlencode($mail_body);?>">Send Email</a>
 
